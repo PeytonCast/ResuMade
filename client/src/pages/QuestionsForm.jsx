12d8790cdc055154e7ea4281f200908f5f502959
miss-mad <mmlh.harris@gmail.com>
2022-12-06 22:20:29 -0500

    questionsform testing

--- client/src/pages/QuestionsForm.jsx
+++ client/src/pages/QuestionsForm.jsx
@@ -2,8 +2,12 @@ import React from "react";
 // either use Formik, Stepper or Yup
 
 export default function QuestionsForm() {
-    return (
-        <>
-        </>
-    )
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  return (
+    <section>
+      <h1>Hello {firstName}{lastName}</h1>
+    </section>
+  );
 }
