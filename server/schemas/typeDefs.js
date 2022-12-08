2999653110c08c3452007ffdf16034bc26bd8aac
Peyton C <peyton.castill0@icloud.com>
2022-12-08 17:00:01 -0500

    addin auth and typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,175 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        resumes: [Resume]
+    }
+
+    type Resume: {
+        _id: ID!
+        personal_info: {
+            firstName: String!
+    
+            lastName: String!
+            
+            city: String!
+    
+            state: String!
+    
+            phoneNumber: String!
+    
+            email: String!
+    
+            github: String!
+    
+            linkedin: String!
+            
+            phoneNumber: String!
+            
+        }
+    
+        summary: String
+    
+        skills: {
+            languages: [String]
+            frameworks: [String]
+            libraries:[String]
+            concepts: [String]
+        },
+    
+        projects: [
+            {
+                name: String
+                github: String
+                name: String
+                deployment: String
+                summary: String
+                responsibility: String
+                technologies: [String]
+    
+            }
+        ]
+    
+        experiences: [
+            {
+                isCurrent: Boolean
+                title: String
+                company: String
+                city: String
+                state: String
+                summary: String
+                startDate:{
+                    month: Number
+                    year: Number
+                  }
+            }
+        ]
+
+        educations: [
+            {
+                degree: String
+                fieldOfStudy: String
+                schoolName: String
+                startDate:{
+                    month: Number
+                    year: Number
+                  }
+                endDate:{
+                    month: Number
+                    year: Number
+                  }
+                
+            }
+        ]
+    
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+      }
+    
+    type Query {
+        me: User
+      }
+
+
+    input inputResume {
+        personal_info: {
+            firstName: String!
+            lastName: String!
+            city: String!
+            state: String!
+            phoneNumber: String!
+            email: String!
+            github: String!
+            linkedin: String!
+            phoneNumber: String!
+        }
+
+        summary: String
+
+        skills: {
+            languages: [String]
+            frameworks: [String]
+            libraries:[String]
+            concepts: [String]
+        },
+
+        projects: [
+            {
+                name: String
+                github: String
+                name: String
+                deployment: String
+                summary: String
+                responsibility: String
+                technologies: [String]
+            }
+        ]
+    
+        experiences: [
+            {
+                isCurrent: Boolean
+                title: String
+                company: String
+                city: String
+                state: String
+                summary: String
+                startDate:{
+                    month: Number
+                    year: Number
+                  }
+            }
+        ]
+
+        educations: [
+            {
+                degree: String
+                fieldOfStudy: String
+                schoolName: String
+                startDate:{
+                    month: Number
+                    year: Number
+                  }
+                endDate:{
+                    month: Number
+                    year: Number
+                  }
+            }
+        ]
+    }
+
+    type Mutation{ 
+        login(email: String!, password: String!): Auth
+        saveResume(resumeData: inputResume!): User
+        addUser(username: String!, email: String!, password: String!): Auth
+        removeBook(_id : ID!): User
+    }
 
 `;
 
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,8 +1,37 @@
 const jwt = require('jsonwebtoken');
-const config = require('../config/config')
-const secret = config.jwtSecretKey;
-const expiration = config.jwtExpiration;
-
+// const config = require('../config/config')
+require('dotenv').config()
+const secret = process.env.SECRET
+const expiration = '24h';
 module.exports = {
+  // function for our authenticated routes
+  authMiddleware: function ({req}) {
+    // allows token to be sent via  req.query or headers
+    let token = req.body.token ||req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+     return req;
+    
   
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
 };
