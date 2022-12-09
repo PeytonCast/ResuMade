e3799a8d6e487df003f0a797129c2c8ac1c4f67a
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-12-09 15:17:31 -0500

    finished main layout for homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,26 +1,38 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "../index.css";
-import { Jumbotron } from "react-bootstrap";
-import { Col, Row } from "antd";
+import { Link } from "react-router-dom";
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 const Homepage = () => {
   return (
     <div>
-      <Jumbotron>
-        <div className="hero">
-          <div className="hero-text d-flex justify-content-center align-items-center h-100">
-            <h1>Welcome to ResuMade</h1>
-            <p>Easily create your resume here</p>
+      <div>
+          <div className="hero">
+            <div className="hero-content">
+              <h1 className="title">Welcome to ResuMade</h1>
+              <p className="subtitle">Easily create professional resumes here</p>
+            </div>
           </div>
-        </div>
-      </Jumbotron>
+      </div>
       <div className="example-container">
-        <Row>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
+        <Container>
+          <Row>
+            <Col sm={4}>
+              <img className="example-img" src="https://images.unsplash.com/reserve/LJIZlzHgQ7WPSh5KVTCB_Typewriter.jpg" alt="example"></img>
+            </Col>
+            <Col sm={8}>
+              <h3>Need a resume?</h3>
+              <p style={{ fontStyle: "italic"}}>"A resume is an important tool for your job search as it offers a page or two where you can display your relevant skills and qualities for a job. Resumes help employers make hiring decisions and help you get your first interview. That's why it matters how you structure your resume and what information you decide to include."</p>
+              <p>Not sure how to write a resume or tired of creating your own? Just fill out our questionnaire and we'll do the work for you. </p>
+              <p><Link to="/login" className="link">Login to get started.</Link></p>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );
