1e48a1da97947c451d598d95d878b32cd7f0786a
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-12-08 23:41:48 -0500

    created hero jumbotron, render nav and home on app.js

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,6 +1,29 @@
 import React from "react";
+import "../index.css";
+import { Jumbotron } from "react-bootstrap";
+import { Col, Row } from "antd";
 
 const Homepage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Jumbotron>
+        <div className="hero">
+          <div className="hero-text d-flex justify-content-center align-items-center h-100">
+            <h1>Welcome to ResuMade</h1>
+            <p>Easily create your resume here</p>
+          </div>
+        </div>
+      </Jumbotron>
+      <div className="example-container">
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+      </div>
+    </div>
+  );
 };
+
 export default Homepage;
