41c51501af78e5601dd2048b8a05339df786f236
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-12-08 20:33:59 -0500

    remade nav and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
-//import "./App.css";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Homepage from "../src/pages/Homepage";
+import Dashboard from "../src/pages/Dashboard";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
 
 //import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,113 +1,41 @@
-import Homepage from "../pages/Homepage";
-import Dashboard from "../pages/Dashboard";
-import LoginForm from "./LoginForm";
-import SignupForm from "./SignupForm";
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
-import { Routes, Route, useNavigate } from "react-router-dom";
-import React, { useState } from "react";
-import { Menu, Drawer } from "antd";
-import { MenuOutlined } from "@ant-design/icons";
-import "antd/dist/reset.css";
-import "../index.css";
+//TODO: need to figure out way to conditionally render based on user logged in (isAuthenticated?)
 
-function Navbar() {
-  const [openMenu, setOpenMenu] = useState(false); // state for opening drawer(side menu)
+export default function Navbar({ loggedIn, setLoggedIn }) {
   return (
-    <div style={{ height: "100vh", backgroundColor: "#E0E2DB" }}>
-      <div
-        style={{
-          backgroundColor: "#000000",
-          height: 60,
-          paddingLeft: 12,
-          paddingTop: 12,
-        }}
-        className="menuIcon">
-        <MenuOutlined
-          style={{
-            color: "white",
-            fontSize: 20,
-          }}
-          onClick={() => {
-            setOpenMenu(true);
-          }}
-        />
-      </div>
-      <div></div>
-      <span className="headerMenu">
-        <AppMenu />
-      </span>
-      <Drawer // side menu for mobile view
-        open={openMenu}
-        onClose={() => {
-          setOpenMenu(false);
-        }}
-        closable={false}
-        bodyStyle={{ backgroundColor: "#E0E2DB" }}>
-        <AppMenu isInline />
-      </Drawer>
-    </div>
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        ResuMade
+      </Link>
+      <ul>
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/dashboard">Account</CustomLink>
+        {loggedIn ? (
+          <CustomLink to="/signout" onClick={() => setLoggedIn(!loggedIn)}>
+            Signout
+          </CustomLink>
+        ) : (
+          <CustomLink to="/login" onClick={() => setLoggedIn(!loggedIn)}>
+            Login
+          </CustomLink>
+        )}
+        {loggedIn ? "" : <CustomLink to="/signup">Signup</CustomLink>}
+      </ul>
+    </nav>
   );
 }
 
-// nav menu
-function AppMenu({ isInline = false }) {
-  const navigate = useNavigate();
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
   return (
-    <div style={{ height: "120vh", backgroundColor: "#E0E2DB" }}>
-      <Menu
-        onClick={({ key }) => {
-          if (key === "signout") {
-            //TODO sign out feature here
-          } else {
-            navigate(key);
-          }
-        }}
-        style={{
-          backgroundColor: "#000000",
-          color: "#E5E5E5",
-          fontSize: 18,
-          border: "none",
-          justifyContent: "flex-end",
-        }}
-        mode={isInline ? "inline" : "horizontal"}
-        items={[
-          {
-            label: "Home",
-            key: "home",
-          },
-          {
-            label: "Account",
-            key: "dashboard",
-          },
-          {
-            label: "Login",
-            key: "login",
-          },
-          {
-            label: "Signup",
-            key: "signup",
-          },
-          // {
-          //   label: "Signout",
-          //   key: "signout",
-          // },
-        ]}></Menu>
-      <Content />
-    </div>
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
   );
 }
-
-function Content() {
-  <div>
-    <Routes>
-      <Route path="/" element={<Homepage />}></Route>
-      <Route path="/dashboard" element={<Dashboard />}></Route>
-      <Route path="/login" element={<LoginForm />}></Route>
-      <Route path="/signup" element={<SignupForm />}></Route>
-      {/* <Route path="/signout" element={<Signout />}></Route> */}
-    </Routes>
-    ;
-  </div>;
-}
-
-export default Navbar;
