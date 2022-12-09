9a46fe5303caf67928581063f8c8cf808fd52436
miss-mad <mmlh.harris@gmail.com>
2022-12-08 21:19:28 -0500

    setup for 6 parts of the form folder within the components using antd

---
+++ client/src/components/Forms/Experience.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+
+const Experience = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    ></Form>
+  );
+};
+export default Experience;
---
+++ client/src/components/Forms/Projects.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+
+const Projects = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+    </Form>
+  );
+};
+export default Projects;
