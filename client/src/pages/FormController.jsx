87d7788c8711a6c5cfa17a965011da3abaeb2c65
miss-mad <mmlh.harris@gmail.com>
2022-12-08 21:23:22 -0500

    controller for the 6 form sections + uses antd step component to let form be broken down into sections

---
+++ client/src/pages/FormController.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { Row, Col, Steps, Button, message } from "antd";
+import {
+  UserInfo,
+  Summary,
+  TechnicalSkills,
+  Projects,
+  Experience,
+  Education,
+} from "../components/Forms";
+
+const FormController = () => {
+  const [current, setCurrent] = useState(0);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
+  const steps = [
+    {
+      title: "Personal Info",
+      content: <UserInfo />,
+    },
+    {
+      title: "Summary",
+      content: <Summary />,
+    },
+    {
+      title: "Technical Skills",
+      content: <TechnicalSkills />,
+    },
+    {
+      title: "Projects",
+      content: <Projects />,
+    },
+    {
+      title: "Experience",
+      content: <Experience />,
+    },
+    {
+      title: "Education",
+      content: <Education />,
+    },
+  ];
+
+  const items = steps.map((item) => ({
+    key: item.title,
+    title: item.title,
+  }));
+
+  return (
+    <Row justify="center" align="middle">
+      <Col>
+        <Steps current={current} items={items} />
+        <div className="steps-content">{steps[current].content}</div>
+        <div className="steps-action">
+          {current < steps.length - 1 && (
+            <Button type="primary" onClick={() => next()}>
+              Next
+            </Button>
+          )}
+
+          {current === steps.length - 1 && (
+            <Button
+              type="primary"
+              onClick={() => message.success("Processing complete!")}
+            >
+              Done
+            </Button>
+          )}
+
+          {current > 0 && (
+            <Button
+              style={{
+                margin: "0 8px",
+              }}
+              onClick={() => prev()}
+            >
+              Previous
+            </Button>
+          )}
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+export default FormController;
