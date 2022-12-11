0fa00b88abf48b1289900d21b7d8dea583b44380
melissa <melissastan91@gmail.com>
2022-12-10 23:30:29 -0500

    client side and apollo are still connected

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,12 +7,12 @@ import { QUERY_ME} from '../utils/queries';
 
 //user can add, delete, edit resume
 const Dashboard = () => {
-  // const {data, loading} = useQuery(QUERY_ME);
-  // // const [addResume] = useMutation(ADD_RESUME)
+  const {data, loading} = useQuery(QUERY_ME);
+  // const [addResume] = useMutation(ADD_RESUME)
 
-  // const user = data?.me || data?.user || {};
+  const user = data?.me || data?.user || {};
  
-  // console.log(data)
+  console.log(data)
 
   //COMMENT BACK IN WHEN AUTH LOGIN IS SETUP
     // if (loading) {
