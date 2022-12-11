56bffc3f140268f94602e1b27510b5c180c19f2d
melissa <melissastan91@gmail.com>
2022-12-10 23:09:34 -0500

    client side can connect to apollo

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,61 +1,42 @@
 import React from "react";
-import Auth from '../utils/auth';
-import { useMutation, useQuery } from '@apollo/client';
-import { useParams } from 'react-router-dom';
+import CardResume from '../components/CardResume';
+// import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
+// import { useParams } from 'react-router-dom';
+import { QUERY_ME} from '../utils/queries';
 
-const Profile = () => {
-   
-  
-  const { username: userParam } = useParams();
-
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
-  });
-
-  const user = data?.me || data?.user || {};
-  // navigate to personal profile page if username is yours
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Navigate to="/me" />;
-  }
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!user?.username) {
-    return (
-      <h4>
-        You need to be logged in to see this. Use the navigation links above to
-        sign up or log in!
-      </h4>
-    );
-  }
-}
-
+//user can add, delete, edit resume
 const Dashboard = () => {
-  const [userData, setUserData] = useState({});
-  const {data, loading} = useQuery(GET_ME);
-  const [addResume] = useMutation(SAVE_RESUME)
-  const { username: userParam } = useParams();
+  // const {data, loading} = useQuery(QUERY_ME);
+  // // const [addResume] = useMutation(ADD_RESUME)
 
+  // const user = data?.me || data?.user || {};
+ 
+  // console.log(data)
 
-
-    if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-      return <Navigate to="/dashboard" />;
-    }
+  //COMMENT BACK IN WHEN AUTH LOGIN IS SETUP
+    // if (loading) {
+    //   return <div>Loading...</div>;
+    // }
   
-    if (loading) {
-      return <div>Loading...</div>;
-    }
+    // if (!user?.username) {
+    //   return (
+    //     <h4>
+    //       You need to be logged in to see this. Use the navigation links above to
+    //       sign up or log in!
+    //     </h4>
+    //   );
+    // }
+
+    // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    //   return <Navigate to="/dashboard" />;
+    // }
   
-    if (!user?.username) {
-      return (
-        <h4>
-          You need to be logged in to see this. Use the navigation links above to
-          sign up or log in!
-        </h4>
-      );
-    }
+
+    return (
+      <div>
+         <CardResume />
+      </div>
+    );
 };
 export default Dashboard;
