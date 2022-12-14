f63f9d09baa8fb8b4ba68a70eaf5d21b24c0d020
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-12-14 03:12:36 -0500

    updated homepage styling with antD, updated nav to antD, added Auth.loggedIn checks for account and forms page

--- client/src/pages/FormController.jsx
+++ client/src/pages/FormController.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
+
 import { Row, Col, Steps, Button, message, Form } from "antd";
 import {
   UserInfo,
@@ -10,12 +13,14 @@ import {
   Preview,
 } from "../components/Forms";
 
-import { loadStripe  } from "@stripe/stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { QUERY_CHECKOUT } from "../utils/queries";
-import { useLazyQuery } from '@apollo/client';
-import './formController.css'
-const resume = require('../components/Templates/resumedata');
-const stripePromise = loadStripe("pk_test_51MEcXfKCu6tOY76M3glH98vnG12XLfoyY7tA9sT5APZOwtj6LnhXMPiatC5I8BealmLrL3ejoUoLVU2Se51Caoty00ul1ZAgr5");
+import { useLazyQuery } from "@apollo/client";
+import "./formController.css";
+const resume = require("../components/Templates/resumedata");
+const stripePromise = loadStripe(
+  "pk_test_51MEcXfKCu6tOY76M3glH98vnG12XLfoyY7tA9sT5APZOwtj6LnhXMPiatC5I8BealmLrL3ejoUoLVU2Se51Caoty00ul1ZAgr5"
+);
 
 const FormController = () => {
   const [current, setCurrent] = useState(0);
@@ -57,7 +62,7 @@ const FormController = () => {
     },
     {
       title: "Preview",
-      content: <Preview resume={resume}/>
+      content: <Preview resume={resume} />,
       // content: < />, // add <Preview/> here when it's ready
     },
   ];
@@ -70,65 +75,76 @@ const FormController = () => {
   useEffect(() => {
     console.log(userData);
   });
-  
+
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
     if (data) {
       stripePromise.then((res) => {
-        res.redirectToCheckout({sessionId: data.checkout.session });
+        res.redirectToCheckout({ sessionId: data.checkout.session });
       });
     }
-  }, [data])
+  }, [data]);
 
   const doneBtnHandler = async () => {
     getCheckout();
-  }
+  };
 
   return (
-    <div className="flex-container flex-row">
-    <Row justify="center" align="middle">
-      <Col className="FormContainer">
-        <Steps current={current} items={items} />
+    <>
+      {Auth.loggedIn() ? (
+        <div className="flex-container flex-row">
+          <Row justify="center" align="middle">
+            <Col className="FormContainer">
+              <Steps current={current} items={items} />
 
-        <div className="steps-content">
-          <Form form={form}>{steps[current].content}</Form>
+              <div className="steps-content">
+                <Form form={form}>{steps[current].content}</Form>
+              </div>
+
+              <div className="steps-action">
+                {current > 0 && (
+                  <Button
+                    style={{
+                      margin: "0 8px",
+                    }}
+                    onClick={() => prev()}>
+                    Previous
+                  </Button>
+                )}
+                {current < steps.length - 1 && (
+                  <Button type="primary" onClick={() => next()}>
+                    Next
+                  </Button>
+                )}
+
+                {current === steps.length - 1 && (
+                  <Button
+                    type="primary"
+                    onClick={() => {
+                      form.submit();
+                      setUserData(form.getFieldsValue(true));
+                      message.success("Your ResuMate is ready to download!");
+                      doneBtnHandler();
+                    }}>
+                    Done
+                  </Button>
+                )}
+              </div>
+            </Col>
+          </Row>
         </div>
-
-        <div className="steps-action">
-          {current > 0 && (
-            <Button
-              style={{
-                margin: "0 8px",
-              }}
-              onClick={() => prev()}
-            >
-              Previous
-            </Button>
-          )}
-          {current < steps.length - 1 && (
-            <Button type="primary" onClick={() => next()}>
-              Next
-            </Button>
-          )}
-
-          {current === steps.length - 1 && (
-            <Button
-              type="primary"
-              onClick={() => {
-                form.submit();
-                setUserData(form.getFieldsValue(true));
-                message.success("Your ResuMate is ready to download!");
-                doneBtnHandler();
-              }}
-            >
-              Done
-            </Button>
-          )}
-        </div>
-      </Col>
-    </Row>
-    </div>
+      ) : (
+        <>
+          <h2 style={{ textAlign: "center" }}>
+            You must be logged in to access this page!{" "}
+          </h2>
+          <p style={{ textAlign: "center" }}>
+            Click <Link to="/login">here</Link> to go to the login page.
+          </p>
+        </>
+      )}
+    </>
   );
 };
 
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,73 +1,60 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "../index.css";
 import { Link } from "react-router-dom";
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
 import Auth from "../utils/auth";
 
-import { Carousel } from "antd";
-
-// const contentStyle = {
-//   height: '160px',
-//   color: '#fff',
-//   lineHeight: '160px',
-//   textAlign: 'center',
-//   background: '#364d79',
-// };
+import { Carousel, Col, Row } from "antd";
 
 const Homepage = () => {
   return (
     <div>
-        <div className="hero">
-          <div className="hero-content">
-            <h1 className="title">Welcome to ResuMade</h1>
-            <p className="subtitle">Easily create professional resumes here</p>
-            <a href="#learn-how" id="learn-link">Learn how</a>
+      <div className="hero">
+        <div className="hero-content">
+          <h1 className="title">Welcome to ResuMade</h1>
+          <p className="subtitle">Easily create professional resumes here</p>
+          <a href="#learn-how" id="learn-link">
+            Learn how
+          </a>
         </div>
-    </div>
+      </div>
       <div className="motto-container">
-        <Container>
-          <Row>
-            <Col sm={4}>
-              <img
-                className="motto-img"
-                src="https://images.unsplash.com/reserve/LJIZlzHgQ7WPSh5KVTCB_Typewriter.jpg"
-                alt="typewriter"></img>
-            </Col>
-            <Col sm={8}>
-              <h3>Need a resume?</h3>
-              <p style={{ fontStyle: "italic" }}>
-                "A resume is an important tool for your job search as it offers
-                a page or two where you can display your relevant skills and
-                qualities for a job. Resumes help employers make hiring
-                decisions and help you get your first interview. That's why it
-                matters how you structure your resume and what information you
-                decide to include."
-              </p>
+        <Row>
+          <Col span={6} offset={4} xs={20} sm={16} xl={6}>
+            <img
+              className="motto-img"
+              src="https://images.unsplash.com/reserve/LJIZlzHgQ7WPSh5KVTCB_Typewriter.jpg"
+              alt="typewriter"></img>
+          </Col>
+          <Col span={8} xs={{ span: 20, offset: 2 }} sm={20} xl={8}>
+            <h2>Need a resume?</h2>
+            <p style={{ fontStyle: "italic" }}>
+              "A resume is an important tool for your job search as it offers a
+              page or two where you can display your relevant skills and
+              qualities for a job. Resumes help employers make hiring decisions
+              and help you get your first interview. That's why it matters how
+              you structure your resume and what information you decide to
+              include."
+            </p>
+            <p>
+              Not sure how to write a resume or tired of creating your own? Just
+              fill out our questionnaire and we'll do the work for you.{" "}
+            </p>
+            {Auth.loggedIn() ? (
               <p>
-                Not sure how to write a resume or tired of creating your own?
-                Just fill out our questionnaire and we'll do the work for you.{" "}
+                <Link to="/dashboard" id="dash-link">
+                  Go to your dashboard to create a new resume
+                </Link>
               </p>
-              {Auth.loggedIn() ? (
-                <p>
-                  <Link to="/dashboard" id="dash-link">
-                    Go to your dashboard to create a new resume
-                  </Link>
-                </p>
-              ) : (
-                <p>
-                  <Link to="/login" id="start-link">
-                    Log in to get started
-                  </Link>
-                </p>
-              )}
-            </Col>
-          </Row>
-        </Container>
+            ) : (
+              <p>
+                <Link to="/login" id="start-link">
+                  Log in to get started
+                </Link>
+              </p>
+            )}
+          </Col>
+        </Row>
       </div>
       <div className="preview-section" id="learn-how">
         {/* <Container>
@@ -99,30 +86,46 @@ const Homepage = () => {
         <div className="carousel-css">
           <Carousel autoplay>
             <div>
-              <h4 style={{padding: '50px'}}>Create an account to save your information</h4>
+              <h2 style={{ padding: "50px" }}>
+                1. Create an account to save your information
+              </h2>
               <div className="carousel-img">
-              <img src="https://uploads-ssl.webflow.com/6084b8de4ac99398870ea634/6097b9174eb64608cbcb5a08_gpfTiueUTZym2xzjJI7q.png" style={{height: '400 px', width: "500 px"}} alt="form"></img>
+                <img
+                  src="https://uploads-ssl.webflow.com/6084b8de4ac99398870ea634/6097b9174eb64608cbcb5a08_gpfTiueUTZym2xzjJI7q.png"
+                  style={{ height: "400 px", width: "500 px" }}
+                  alt="form"></img>
               </div>
             </div>
             <div>
-              <h4 style={{padding: '50px'}}>Fill out the questionnaire</h4>
+              <h2 style={{ padding: "50px" }}>2. Fill out the questionnaire</h2>
               <div className="carousel-img">
-              <img src="https://uploads-ssl.webflow.com/6084b8de4ac99398870ea634/6097b9174eb64608cbcb5a08_gpfTiueUTZym2xzjJI7q.png" style={{height: '400 px', width: "500 px"}} alt="form"></img>
+                <img
+                  src="https://uploads-ssl.webflow.com/6084b8de4ac99398870ea634/6097b9174eb64608cbcb5a08_gpfTiueUTZym2xzjJI7q.png"
+                  style={{ height: "400 px", width: "500 px" }}
+                  alt="form"></img>
               </div>
             </div>
             <div>
-              <h4 style={{padding: '50px'}}> Save and quit anytime</h4>
+              <h2 style={{ padding: "50px" }}>3. Save and quit anytime</h2>
               <div className="carousel-img">
-              <img src="https://uploads-ssl.webflow.com/6084b8de4ac99398870ea634/6097b9174eb64608cbcb5a08_gpfTiueUTZym2xzjJI7q.png" style={{height: '400 px', width: "500 px"}} alt="form"></img>
+                <img
+                  src="https://uploads-ssl.webflow.com/6084b8de4ac99398870ea634/6097b9174eb64608cbcb5a08_gpfTiueUTZym2xzjJI7q.png"
+                  style={{ height: "400 px", width: "500 px" }}
+                  alt="form"></img>
               </div>
             </div>
             <div>
-              <h4 style={{padding: '50px'}}>Preview and download your generated resume after completing all the questions!</h4>
+              <h2 style={{ padding: "50px" }}>
+                4. Preview and download your generated resume after completing
+                all the questions!
+              </h2>
               <div className="carousel-img">
-              <img src="https://binaries.templates.cdn.office.net/support/templates/en-us/lt16402488_quantized.png" style={{height: '400 px', width: "500 px"}} alt="form"></img>
+                <img
+                  src="https://binaries.templates.cdn.office.net/support/templates/en-us/lt16402488_quantized.png"
+                  style={{ height: "400 px", width: "500 px" }}
+                  alt="form"></img>
               </div>
             </div>
-           
           </Carousel>
         </div>
       </div>
