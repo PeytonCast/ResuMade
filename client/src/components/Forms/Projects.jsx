7b6cdf6db6ed367c662373e00474f7f1f69f70b0
miss-mad <mmlh.harris@gmail.com>
2022-12-16 09:37:08 -0500

    added 3 projects to meet industry standard

--- client/src/components/Forms/Projects.jsx
+++ client/src/components/Forms/Projects.jsx
@@ -98,6 +98,124 @@ const Projects = () => {
         {/* upon check, add a duplicate project section to add additional project */}
         <Checkbox></Checkbox>
       </Form.Item>
+
+      <Row>
+        <Col span={20}>
+          <Form.Item label="Project Name" name="projectName">
+            <Input maxLength={15} />
+          </Form.Item>
+        </Col>
+
+        {/* <Col span={4}>
+          <Form.Item
+            label="Collaborative?"
+            name="collaborative"
+            valuePropName="checked"
+          >
+            <Checkbox></Checkbox>
+          </Form.Item>
+        </Col> */}
+      </Row>
+
+      <Row>
+        <Col span={12}>
+          <Form.Item label="Github Repo Link" name="githubRepoLink">
+            <Input addonBefore="https://" placeholder="Github" />
+          </Form.Item>
+        </Col>
+
+        <Col span={12}>
+          <Form.Item
+            label="Deployed Application Link"
+            name="deployedApplicationLink"
+          >
+            <Input addonBefore="https://" />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Form.Item label="Project Description" name="projectDescription">
+        <TextArea rows={2} />
+      </Form.Item>
+
+      <Form.Item label="Your Role" name="yourRole">
+        <TextArea rows={2} />
+      </Form.Item>
+
+      {/* note to ask user to separate each item with a comma and a space */}
+      {/* array of strings */}
+      <Form.Item label="Tools and Technologies Used" name="toolsTechnologies">
+        <TextArea rows={4} />
+      </Form.Item>
+
+      {/* need a spot to emphasize that resumes should have 3-5 projects, spotlighting collaborative projects first (not MVP) */}
+      <Form.Item
+        label="Add Another"
+        name="addAnotherProject"
+        valuePropName="checked"
+      >
+        {/* upon check, add a duplicate project section to add additional project */}
+        <Checkbox></Checkbox>
+      </Form.Item>
+
+      <Row>
+        <Col span={20}>
+          <Form.Item label="Project Name" name="projectName">
+            <Input maxLength={15} />
+          </Form.Item>
+        </Col>
+
+        {/* <Col span={4}>
+          <Form.Item
+            label="Collaborative?"
+            name="collaborative"
+            valuePropName="checked"
+          >
+            <Checkbox></Checkbox>
+          </Form.Item>
+        </Col> */}
+      </Row>
+
+      <Row>
+        <Col span={12}>
+          <Form.Item label="Github Repo Link" name="githubRepoLink">
+            <Input addonBefore="https://" placeholder="Github" />
+          </Form.Item>
+        </Col>
+
+        <Col span={12}>
+          <Form.Item
+            label="Deployed Application Link"
+            name="deployedApplicationLink"
+          >
+            <Input addonBefore="https://" />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Form.Item label="Project Description" name="projectDescription">
+        <TextArea rows={2} />
+      </Form.Item>
+
+      <Form.Item label="Your Role" name="yourRole">
+        <TextArea rows={2} />
+      </Form.Item>
+
+      {/* note to ask user to separate each item with a comma and a space */}
+      {/* array of strings */}
+      <Form.Item label="Tools and Technologies Used" name="toolsTechnologies">
+        <TextArea rows={4} />
+      </Form.Item>
+
+      {/* need a spot to emphasize that resumes should have 3-5 projects, spotlighting collaborative projects first (not MVP) */}
+      <Form.Item
+        label="Add Another"
+        name="addAnotherProject"
+        valuePropName="checked"
+      >
+        {/* upon check, add a duplicate project section to add additional project */}
+        <Checkbox></Checkbox>
+      </Form.Item>
     </>
   );
 };
