db1aaa1429771696088ae9ba0bd3d9d8155c08ec
melissa <melissastan91@gmail.com>
2022-12-17 10:21:07 -0500

    testing

--- server/server.js
+++ server/server.js
@@ -15,10 +15,11 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  formatError: (err) => {
-    console.error(err);
-    return err;
-  },
+  context: authMiddleware
+  // formatError: (err) => {
+  //   console.error(err);
+  //   return err;
+  // },
 });
 // middleware
 app.use(express.urlencoded({ extended: false }));
@@ -26,11 +27,11 @@ app.use(express.json());
  
 // if we're in production, serve client/build as static assets
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// }
 
-app.get("/",(req, res) => {
+app.get("/*",(req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
