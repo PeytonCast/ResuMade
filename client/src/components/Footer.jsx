e2f4b1716a1e51cd2d4885c063b90b3fd6358ca9
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-12-18 20:07:43 -0500

    updated footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,28 +7,17 @@ const FooterBar = () => {
     <div className="main-footer">
       <div className="container">
         <Row justify="space-around">
-          {/* Column1 */}
           <Col span={18}>
             <h4>Disclaimer:</h4>
             <p>
-              ResuMade is not responsible for any rejected job offers. However,
-              we can guarantee that our resume templates will give you the{" "}
-              <span style={{ fontWeight: "bold" }}>best</span> asset to start
-              your career journey.
+              Our template is specifically made for those seeking employment or
+              advancement in the coding/web development industry. Certain
+              questions within this form would not be applicable to other
+              industries. This website focuses on the resume formatting and
+              content only and does not guarantee job placement or interview
+              scheduling with any company. We hope this helps you!
             </p>
           </Col>
-          {/* Column2 */}
-          {/* <Col span={8}>
-            <h4>Contact Us</h4>
-            <ui style={{ listStyle: "none" }}>
-              <li>
-                <a href="">LinkedIn</a>
-              </li>
-              <li>
-                <a href="https://github.com/PeytonCast/ResuMade">GitHub</a>{" "}
-              </li>
-            </ui>
-          </Col> */}
         </Row>
         <hr />
         <div>
